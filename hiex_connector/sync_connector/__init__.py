2be5ab9f5e54ab368b60862a1c89c2f3985a39a7
Floms <jeka.floms@gmail.com>
2022-09-29 20:17:37 +0300

    Помилки тепер у форматі виключень

--- hiex_connector/sync_connector/__init__.py
+++ hiex_connector/sync_connector/__init__.py
@@ -5,8 +5,6 @@ from hiex_connector.types import *
 class HiExConnector(HiExConnectorBase):
     def admin_coins_list(self):
         resp = self.get_request('admin/coins/list', {})
-        if isinstance(resp, ResponseException):
-            return resp
         coins = []
         for coin in resp['coins']:
             coins.append(Coin(**coin))
@@ -17,15 +15,11 @@ class HiExConnector(HiExConnectorBase):
             'exchange_id': exchange_id,
             'step': step,
         })
-        if isinstance(resp, ResponseException):
-            return resp
         return Exchange(**resp['exchange'])
 
     def admin_exchanges_list(self):
         resp = self.get_request('admin/exchanges/list', {
         })
-        if isinstance(resp, ResponseException):
-            return resp
         exchanges = []
         for exchange in resp['exchanges']:
             exchanges.append(Exchange(**exchange))
