b5308af6ac92fcd1075f200a72fbe2cf6791a241
Floms <jeka.floms@gmail.com>
2022-09-29 17:54:16 +0300

    Зробив нормальне відображення (print) типів

--- hiex_connector/types/__init__.py
+++ hiex_connector/types/__init__.py
@@ -1,6 +1,11 @@
 class BaseType:
-    def __getitem__(self, item):
-        return self.values[item]
 
-    def __getattr__(self, item):
-        return self.values[item]
+    def __repr__(self):
+        return (
+            f"<{self.__class__.__name__} values: "
+            + ";".join([f"{key}={self[key]}" for key in self.__dict__.keys()])
+            + ">"
+        )
+
+    def __getitem__(self, item):
+        return getattr(self, item)
