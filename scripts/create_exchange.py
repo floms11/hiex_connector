ed9bf754a6addfbcfa2f0a60bd4e2e6413c2208b
Floms <jeka.floms@gmail.com>
2022-10-02 17:32:28 +0300

    Додав скрипт create_exchange

---
+++ scripts/create_exchange.py
@@ -0,0 +1,26 @@
+import asyncio
+from hiex_connector.magic_async_connector import AsyncHiEMagic
+
+PRIVATE_KEY = '<ПРИВАТНИЙ КЛЮЧ>'
+PUBLIC_KEY = '<ПУБЛІЧНИЙ КЛЮЧ>'
+
+USER_AUTH_KEY = '<КЛЮЧ КОРИСТУВАЧА>'
+
+
+async def main():
+    hiex = AsyncHiEMagic(PRIVATE_KEY, PUBLIC_KEY)
+
+    pairs = await hiex.pairs_list(currency1='USDT_TRC20', currency2='UAH_VISAMASTER')
+    pair = pairs[0]
+
+    user = await hiex.user_get(USER_AUTH_KEY)
+    exchange = await user.exchange_create(pair, '<КАРТКА НА ЯКУ ОТРИМУЄМО КОШТИ>', amount1=10)
+    await exchange.confirm()
+
+    while True:
+        print(exchange)
+        await exchange.reload()
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
+    asyncio.run(main())
