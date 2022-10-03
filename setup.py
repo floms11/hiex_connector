6294e55209185fc3981ebc767b412051d489cf68
Floms <jeka.floms@gmail.com>
2022-10-03 17:32:18 +0300

    Додав setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import re
+
+from setuptools import find_packages, setup
+from hiex_connector.base import HiExConnectorBase
+
+PACKAGE_NAME = 'hiex_connector'
+SOURCE_DIRECTORY = 'hiex_connector'
+SOURCE_PACKAGE_REGEX = re.compile(rf'^{SOURCE_DIRECTORY}')
+
+source_packages = find_packages(include=[SOURCE_DIRECTORY, f'{SOURCE_DIRECTORY}.*'])
+proj_packages = [SOURCE_PACKAGE_REGEX.sub(PACKAGE_NAME, name) for name in source_packages]
+
+setup(
+    name=PACKAGE_NAME,
+    version=HiExConnectorBase.get_version_api(),
+    packages=proj_packages,
+    package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
+    url='https://docs.hiex.io',
+    license='',
+    author='hiex',
+    author_email='support@hiex.io',
+    description=''
+)
