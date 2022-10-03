38c356f43cd79a7fabe9754787413bd00bec4eab
Floms <jeka.floms@gmail.com>
2022-10-03 16:51:59 +0300

    Щось пішло не по плану

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hiex_connector_admin',
     version='1.0',
-    packages=['hiex_connector'],
+    packages=['hiex_connector_admin'],
     url='https://docs.hiex.io',
     license='',
     author='hiex',
