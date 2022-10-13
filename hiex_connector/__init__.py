fd6ea1cfd72ada836414c37b787eec4e16e3b35a
Floms <jeka.floms@gmail.com>
2022-10-14 00:12:38 +0300

    Додав AsyncHiExUserKYCReviewed в __init__.py

--- hiex_connector/__init__.py
+++ hiex_connector/__init__.py
@@ -1,4 +1,4 @@
-from .async_handlers import AsyncHiExNotifications, AsyncHiExExchangeUpdate
+from .async_handlers import AsyncHiExNotifications, AsyncHiExExchangeUpdate, AsyncHiExUserKYCReviewed
 from .async_connector import AsyncHiExConnector
 from .magic_async_connector import AsyncHiExMagic
 from .sync_connector import HiExConnector
